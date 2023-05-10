ab5b285e5cae78ed4b42923f7fb1cda16dfb2db7
Zokemore <reebaljavedkhan1@gmail.com>
2023-05-10 18:42:43 +0530

    First Proper Commit

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Android-Commandline-build-System
-Commandline build snippet for Android for developers who wish to dump gradle and Android Studio's IDE
+Command Line Build Snippet for Android for developers who wish to dump gradle and Android Studio IDE
+
+set $clbs='location of androidSdk in your .bashrc'
+
+
+- Build Snippet in bin contains main set of instructions
+
+- While build file in application folder root contains details of the application
+
+- Use appframework to create new application as it conforms with build snippet
+
+
+## Usage
+- To use snippet
+		''' build link '''
+		''' build compile '''
---
+++ appframework/src/java/com/zokemore/appdemo/MainActivity.java
@@ -0,0 +1,35 @@
+package com.zokemore.appdemo;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView tv=findViewById(R.id.text1);
+        tv.setText("Something");
+    }
+@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+			case R.id.action_close_session:
+				return true;
+			default:
+				return super.onOptionsItemSelected(item);
+		}
+	}
+}
